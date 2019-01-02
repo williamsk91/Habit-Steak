24c62749726c0a0a3fb9a6d92ce384853088d69d
williamsk91 <williamskusnandi@gmail.com>
2019-01-02 09:43:29 +0800

    added .gitignore

---
+++ server/config/keys.js
@@ -0,0 +1,14 @@
+// do not upload
+
+module.exports = {
+    google: {
+        clientID: '1015053533756-ntifukro6dp05icme6ols6c6i7c3vd24.apps.googleusercontent.com',
+        clientSecret: 'VONJTdoRyr3Sr1bmeI5CnWl0',
+    },
+    mongodb: {
+        dbURI: 'mongodb://williamsk:Sephereus9@ds123012.mlab.com:23012/habit'
+    },
+    session: {
+        cookieKey: 'HabitSteakCookieKeyldkasjfcnlahusfbeiuahncljrcaerasdfcn23479n872u3hn1723n'
+    }
+}
